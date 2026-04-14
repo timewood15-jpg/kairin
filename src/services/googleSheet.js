a53d0c79285d545461a8a13f0a405de0f79f481a
Danang <danang.vai@gmail.com>
2026-04-14 08:01:10 +0700

    Sort Tanggal

--- src/services/googleSheet.js
+++ src/services/googleSheet.js
@@ -16,7 +16,7 @@ async function syncSheet(auth, spreadsheetId, transactions) {
     valueInputOption: 'USER_ENTERED',
     resource: {
       values: transactions.map(t => [
-        new Date(t.transacted_at).toLocaleString('id-ID'),
+        new Date(t.transacted_at).toISOString(),
         t.description,
         t.amount,
         t.category
