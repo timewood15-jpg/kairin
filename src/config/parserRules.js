633aad94812f31366e8aa5095285e5fd2e218666
Danang <danang.vai@gmail.com>
2026-06-05 09:22:32 +0700

    feat(parser): expand finance categories

--- src/config/parserRules.js
+++ src/config/parserRules.js
@@ -35,15 +35,31 @@ module.exports = {
     '+'
   ],
   categoryMap: {
-    hutang: ['bayar hutang', 'dibayar hutang', 'cicilan', 'pinjam'],
+    hutang: ['bayar hutang', 'dibayar hutang', 'cicilan', 'angsuran', 'pinjam'],
     tabungan: ['tabungan', 'deposito'],
     transfer: ['transfer ke', 'transfer dari', 'topup', 'isi saldo'],
-    bisnis: ['modal usaha', 'jualan', 'omzet', 'customer'],
+    bisnis: ['modal usaha', 'jualan', 'omzet'],
     refund: ['refund', 'retur barang', 'uang kembali'],
     tunai: ['tunai', 'atm'],
-    makanan: ['makan', 'kopi', 'minum', 'resto'],
+    makanan: [
+      'makan',
+      'kopi',
+      'minum',
+      'resto',
+      'jajan',
+      'snack',
+      'bakso',
+      'ayam',
+      'warung',
+      'mie'
+    ],
     transportasi: ['bensin', 'grab', 'gojek', 'tol'],
     belanja: ['beli', 'shop', 'market'],
-    utilitas: ['listrik', 'air', 'wifi', 'internet']
+    utilitas: ['listrik', 'air', 'wifi', 'internet'],
+    kesehatan: ['dokter', 'klinik', 'rumah sakit', 'berobat', 'apotek'],
+    kendaraan: ['bengkel', 'oli', 'servis motor', 'servis mobil', 'parkir', 'pajak kendaraan'],
+    rumah: ['kontrakan', 'kos', 'sewa rumah', 'perbaikan rumah'],
+    pulsa: ['pulsa', 'kuota', 'paket data'],
+    hiburan: ['bioskop', 'nonton', 'hiburan']
   }
 };
